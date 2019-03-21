e688e0f1c58964bf7bf5f38e7bdaf2b7bab285aa
Bednarik Boldizsar <bbednarik@gmail.com>
2019-03-21 21:06:36 +0100

    added basic number display

--- app/src/main.ino
+++ app/src/main.ino
@@ -12,9 +12,24 @@
 #define COLOR_ORDER GRB
 #define NUM_LEDS 64
 
+// digit definitions
+
 // variables
 
 CRGB leds[NUM_LEDS];
+
+bool DIGIT[][7] = {
+  { 1,1,1,0,1,1,1 },
+  { 0,0,0,0,0,1,1 },
+  { 1,0,1,1,1,0,1 },
+  { 0,0,1,1,1,1,1 },
+  { 0,1,0,1,0,1,1 },
+  { 0,1,1,1,1,1,0 },
+  { 1,1,1,1,1,1,0 },
+  { 0,0,0,0,1,1,1 },
+  { 1,1,1,1,1,1,1 },
+  { 0,1,1,1,1,1,1 }
+};
 
 void setup()
 {
@@ -25,29 +40,41 @@ void setup()
   Serial.begin(115200);
 
   // pin definitions
-  pinMode(LED_PIN, OUTPUT);
   pinMode(BUTTON_PIN, INPUT);
 
   // fastled setup
 	FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS);
 	FastLED.clear();
-	FastLED.setBrightness(50);
+	FastLED.setBrightness(100);
 }
 
-bool x = false;
+void setNumber(byte position, byte number, CRGB color)
+{
+  byte numbers[] = { 0,7,16,23,30,37,44,51 };
+  // see if the position is valid
+  if((position>=0 && position<sizeof(numbers)) && (number>=0 && number<10))
+  {
+    Serial.print("!");
+    for(int i=0;i<7;i++) { leds[numbers[position]+i] = (DIGIT[number][i] ? color : CRGB(0,0,0)); }
+  }
+}
+
+byte cnt=0;
 
 void loop()
 {
-  for(int i=0;i<7;i++)
-  {
-    leds[i].red = (x ? 255 : 0);
-    leds[i].green = 0;
-    leds[i].blue = 0;
+  /*
+  for(int i=0;i<8;i++) {
+    setNumber(i,cnt,CRGB(200,255,200));
   }
+  */
 
-  x=!x;
+  setNumber(1,cnt,CRGB(200,255,200));
 
-  Serial.print(".");
+  cnt++;
+  cnt = (cnt>9) ? 0 : cnt;
+
+  Serial.print(cnt);
 	FastLED.show();
   delay(500);
 }
